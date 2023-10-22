3eecc0d37b897a5b5e4f4636310aa88a27564ba6
MalikNeelam06 <128276286+MalikNeelam06@users.noreply.github.com>
2023-10-22 14:24:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+let textn=document.getElementById("text");
+let numbert=document.getElementById("delay");
+let button=document.getElementById("btn");
+let divd=document.getElementById("output");
+async function displayf(textv,delay) {
+	try{
+		await new Promise((resolve)=>{
+			setTimeout(()=>{
+				divd.textContent=textv;
+			},delay);
+		})
+	}
+	catch{
+		
+	}
+	
+}
+button.addEventListener("click",(e)=>{
+	e.preventDefault();
+	let textv=textn.value;
+	let delay=parseInt(numbert.value)*1000;
+	displayf(textv,delay);
+})
